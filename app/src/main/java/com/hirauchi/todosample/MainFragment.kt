0de4f912482656f40ed159284e4195c47b470dcc
hirauchg <gentahirauchi@gmail.com>
2020-04-18 19:40:45 +0900

    【Kotlin MVP】MVPアーキテクチャで、TODOアプリを作成する #1 FABの実装

--- app/src/main/java/com/hirauchi/todosample/MainFragment.kt
+++ app/src/main/java/com/hirauchi/todosample/MainFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
 
@@ -14,5 +16,13 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        addTaskButton.setOnClickListener {
+            Toast.makeText(activity, "Add Tapped", Toast.LENGTH_SHORT).show()
+        }
     }
 }
