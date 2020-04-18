5416b71494c432088e7b28939b448b98d0b51c45
hirauchg <gentahirauchi@gmail.com>
2020-04-18 17:06:37 +0900

    【Kotlin MVP】MVPアーキテクチャで、TODOアプリを作成する #1 Toolbarの実装

--- app/src/main/java/com/hirauchi/todosample/MainActivity.kt
+++ app/src/main/java/com/hirauchi/todosample/MainActivity.kt
@@ -2,11 +2,17 @@ package com.hirauchi.todosample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar.also {
+            it.setTitleTextColor(getColor(R.color.white))
+            setSupportActionBar(it)
+        }
     }
 }
